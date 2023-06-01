4a6a73f4fa24363b3a68170c8c36736cc61f5f04
Pedro Valente <callmevalente@gmail.com>
2023-05-31 21:03:10 -0600

    Skeleton file

---
+++ main.py
@@ -0,0 +1,14 @@
+'''
+Future boilerplate
+'''
+
+class Simulator:
+    '''Holds the list of commands and simulator functions'''
+    def __init__(self, com_list):
+        self.com_list = com_list
+
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
