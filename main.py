74eeb6afcfd949cfe0f657d5ac06ed7da72e711d
Connor Barry <cbarry578@gmail.com>
2023-06-01 09:44:40 -0600

    testing

--- main.py
+++ main.py
@@ -10,6 +10,24 @@ class Simulator:
         for i in range(100):
             self.registers[i] = 0;
 
+    def open_file(self, file_name):
+        try:
+            with open(file_name) as input_file:
+                addr = 0 
+                for line in input_file:
+                    line = line.strip()
+                    #make sure its a + or a -
+                    if line[0] == '+' or line[0] == '-':
+                        opcode = line[1:3]       #10
+                        complete_code = line[1:] #1007
+                        if len(complete_code) == 4: #CHECKS TO SEE IF THE TOTAL CODE IS LENGTH OF 4 (EX. 1007)
+                            source = line[3:] #07
+                            self.registers[addr] = complete_code
+                            addr += 1
+            return True
+        except:
+            return False
+
 def main():
     '''Main function'''
     print("Hello World!")
