40c8f4f1191643eecdc752defeab82f74f689a77
nopot22 <npot0222@gmail.com>
2023-06-05 19:08:15 -0600

    Added constructor test

--- test.py
+++ test.py
@@ -1,10 +1,19 @@
 import pytest
 from main import Simulator
+# python -m pytest test.py 
 
-def test_incorrect_file():
+def test_constructor(): #tests the creation of the class
+    insta = Simulator()
+    assert len(insta.registers) == 100
+    assert insta.accumulator == "+0000"
+    assert insta.cur_addr == 0
+    assert len(insta.instructions) == 12
+    assert len(insta.log) == 0
+    
+def test_incorrect_file(): #tests bad file handling
     insta = Simulator()
     assert insta.open_file('BADFILE') == False 
 
-def test_correct_file():
+def test_correct_file(): #tests file handling
     insta = Simulator()
     assert insta.open_file('file1.txt') == True
