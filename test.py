30be1f40e0277af94d47dbb8904e9254ca454160
nopot22 <npot0222@gmail.com>
2023-06-08 22:05:19 -0600

    add use case added

--- test.py
+++ test.py
@@ -1,14 +1,28 @@
 import pytest
-from main import Simulator
+import os
+from main import *
 # python -m pytest test.py 
 
-def test_constructor(): #tests the creation of the class
-    insta = Simulator()
-    assert len(insta.registers) == 100
-    assert insta.accumulator == "+0000"
-    assert insta.cur_addr == 0
-    assert len(insta.instructions) == 12
-    assert len(insta.log) == 0
+def test_add(): #tests the adding use case
+    with open ('file1.txt', 'w') as file:
+        print('+1009', file=file)# read in num
+        print('+1010', file=file) # read in num
+        print('+2009', file=file) # load into accumulator
+        print('+3010', file=file) # add a word in mem to accum
+        print('+2106', file=file) # store a word from accum into memory
+        print('+1106', file=file) # write word from mem onto screen
+    main()
+    os.remove('file1.txt')  
+
+
+
+
+
+
+
+
+
+
     
 def test_incorrect_file(): #tests bad file handling
     insta = Simulator()
@@ -17,3 +31,5 @@ def test_incorrect_file(): #tests bad file handling
 def test_correct_file(): #tests file handling
     insta = Simulator()
     assert insta.open_file('file1.txt') == True
+    
+test_add()
