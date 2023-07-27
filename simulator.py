4706a605067746cba9f5dc0af30589e6b3fc047e
nopot22 <npot0222@gmail.com>
2023-07-27 15:29:50 -0700

    added a six word bit class

--- simulator.py
+++ simulator.py
@@ -35,7 +35,158 @@ BRANCHZERO = 42 Branch to a specific location in memory if the accumulator is ze
 HALT = 43 Pause the program
 '''
 
-'''Simulator Class'''
+'''Simulator  Class'''
+class six_word_simulator:
+    def __init__(self):
+        self.registers = {} #Initializes the registers
+        self.accumulator = "+000000" #Initializes the accumulator
+        self.cur_addr = 0 #Initializes the current address
+        self.console_memory = ""
+        self.instructions = ["000", "010", "011", "020", "021", "030", "031", "032", "033", "040", "041", "042", "043"] #Lists all the valid instructions
+        self.log = [] #Creates a log list of all the operations
+
+        for i in range(250): #Creates the registers using a dictionary
+            self.registers[i] = "+000000"
+            
+    def read(self, addr):
+        '''Reads a word from the keyboard into a specific location in memory.'''
+        self.log.append(f"'{self.console_memory}' was read from keyboard into address: {str(addr).zfill(3)}") #Logs the operation.
+        self.registers[int(addr)] = self.console_memory #Stores the formatted input into the desired register.
+        return True
+        
+    def write(self, addr):
+        '''Writes a word from a specific location in memory to screen.'''
+        self.console_memory = self.registers[int(addr)] #Gets the word from the register.
+        self.log.append(f"{self.console_memory} was written to screen from address: {str(addr).zfill(3)}") #Logs the operation.
+        return True
+        
+    '''Load/store operations'''
+    
+    def load(self, addr):
+        '''Loads a word from a specific location in memory into the accumulator.'''
+        word = self.registers[int(addr)] #Gets the word from the register.
+        self.log.append(f"{word} was loaded to the accumulator from address: {str(addr).zfill(3)}") #Logs the operation.
+        self.accumulator = word #Loads the word into the accumulator.
+        return True
+
+    def store(self, addr):
+        '''Stores a word from the accumulator into a specific location in memory.'''
+        self.log.append(f"{self.accumulator} was stored from the accumulator into address: {str(addr).zfill(3)}") #Logs the operation.
+        self.registers[int(addr)] = self.accumulator #Stores the word from the accumulator into the register.
+        return True
+    
+    '''Arithmetic operations'''
+    
+    def add(self, addr):
+        '''Adds a word from a specific location in memory to the word in the accumulator (leaves the result in the accumulator)'''
+        intial_acc = self.accumulator #Saves the initial value of the accumulator for the log.
+        result = int(self.accumulator) + int(self.registers[int(addr)]) #Adds the word from the register to the word in the accumulator.
+        if result > 999999 or result < -999999: #Checks if the result is too large to be stored in the accumulator.
+            print(f"Overflow error on addition in address {addr}.\nThe result is too large to be stored in the accumulator. The program will now be terminated.")
+            self.log.append(f"Overflow error on addition in address {addr}. The result is too large to be stored in the accumulator.")
+            return False
+        #The following elif and else statements format the result to be stored in the accumulator.
+        elif result > 0: 
+            self.accumulator = f"+{str(result).zfill(6)}" #This could be wrong because it might add two zeros to the beginning of the number
+        elif result == 0:
+            self.accumulator = "+000000"
+        else:
+            self.accumulator = f"{str(result).zfill(6)}" #changing this to be a 6, lets see what happens
+        #It's a long log entry, but is usefull to see what happened.
+        self.log.append(f"{intial_acc} from the accumulator was added to {self.registers[int(addr)]} from address: {str(addr).zfill(3)} and result ({self.accumulator}) was stored back in the accumulator")
+        return True
+
+    def subtract(self, addr):
+        '''Subtracts a word from a specific location in memory from the word in the accumulator (leaves the result in the accumulator)'''
+        intial_acc = self.accumulator #Saves the initial value of the accumulator for the log.
+        result = int(self.accumulator) - int(self.registers[int(addr)]) #Subtracts the word from the register from the word in the accumulator.
+        if result > 999999 or result < -999999: #Checks if the result is too large to be stored in the accumulator.
+            print(f"Overflow error on subtraction in address {addr}.\nThe result is too large to be stored in the accumulator. The program will now be terminated.")
+            self.log.append(f"Overflow error on subtraction in address {addr}. The result is too large to be stored in the accumulator.")
+            return False
+        #The following elif and else statements format the result to be stored in the accumulator.
+        elif result > 0:
+            self.accumulator = f"+{str(result).zfill(6)}"#This could be wrong because it might add two zeros to the beginning of the number
+        elif result == 0:
+            self.accumulator = "+000000"
+        else:
+            self.accumulator = f"{str(result).zfill(6)}"#changing this to be a 6, lets see what happens
+        #It's a long log entry, but is usefull to see what happened.
+        self.log.append(f"{intial_acc} from the accumulator was subtracted by {self.registers[int(addr)]} from address: {str(addr).zfill(3)} and result ({self.accumulator}) was stored back in the accumulator")
+        return True
+
+    def divide(self, addr):
+        '''Divides the word in the accumulator by a word from a specific location in memory (leaves the result in the accumulator).'''
+        intial_acc = self.accumulator #Saves the initial value of the accumulator for the log.
+        result = int(self.accumulator) // int(self.registers[int(addr)]) #Divides the word in the accumulator by the word from the register.
+        #NO DIVISION OPERATION SHOULD RESULT IN OVERFLOW, I'M STILL LEAVING THIS HERE IN CASE THERE IS ANY ABNORMALITY I DIDN'T PREDICT.
+        if result > 999999 or result < -999999: #Checks if the result is too large to be stored in the accumulator.
+            print(f"Overflow error on division in address {addr}.\nThe result is too large to be stored in the accumulator. The program will now be terminated.")
+            self.log.append(f"Overflow error on division in address {addr}. The result is too large to be stored in the accumulator.")
+            return False
+        #The following elif and else statements format the result to be stored in the accumulator.
+        elif result > 0:
+            self.accumulator = f"+{str(result).zfill(6)}"#This could be wrong because it might add two zeros to the beginning of the number
+        elif result == 0:
+            self.accumulator = "+000000"
+        else:
+            self.accumulator = f"{str(result).zfill(6)}"#changing this to be a 6, lets see what happens
+        #It's a long log entry, but is usefull to see what happened.
+        self.log.append(f"{intial_acc} from the accumulator was divided by {self.registers[int(addr)]} from address: {str(addr).zfill(3)} and result ({self.accumulator}) was stored back in the accumulator")
+        return True
+
+    def multiply(self, addr):
+        '''Multiplies a word from a specific location in memory to the word in the accumulator (leaves the result in the accumulator).'''
+        intial_acc = self.accumulator #Saves the initial value of the accumulator for the log.
+        result = int(self.accumulator) * int(self.registers[int(addr)]) #Multiplies the word in the accumulator by the word from the register.
+        if result > 999999 or result < -999999: #Checks if the result is too large to be stored in the accumulator.
+            print(f"Overflow error on multiplication in address {addr}.\nThe result is too large to be stored in the accumulator. The program will now be terminated.")
+            self.log.append(f"Overflow error on multiplication in address {addr}. The result is too large to be stored in the accumulator.")
+            return False
+        #The following elif and else statements format the result to be stored in the accumulator.
+        elif result > 0:
+            self.accumulator = f"+{str(result).zfill(6)}" #This could be wrong because it might add two zeros to the beginning of the number
+        elif result == 0:
+            self.accumulator = "+000000"
+        else:
+            self.accumulator = f"{str(result).zfill(6)}" #changing this to be a 6, lets see what happens
+        #It's a long log entry, but is usefull to see what happened.
+        self.log.append(f"{intial_acc} from the accumulator was multiplied by {self.registers[int(addr)]} from address: {str(addr).zfill(2)} and result ({self.accumulator}) was stored back in the accumulator")
+        return True
+    
+    
+    '''Control operations'''
+
+    def branch(self, addr):
+        '''Branches to a specific location in memory.'''
+        #Sets the current address to the address specified in the instruction.
+        #It subtracts 1 from the desired address since the program moves to next location upon returning.
+        self.cur_addr = int(addr) - 1
+        self.log.append(f"Program branched to address: {str(addr).zfill(3)}") #Logs the operation
+        return True
+    
+    def branch_neg(self, addr):
+        '''Branches to a specific location in memory if the accumulator is negative.'''
+        #Sets the current address to the address specified in the instruction if accumulator is negative.
+        #It subtracts 1 from the desired address since the program moves to next location upon returning.
+        if int(int(self.accumulator)) < 0:
+            self.cur_addr = int(addr) - 1
+            self.log.append(f"Program branched to address: {str(addr).zfill(3)} since the accumulator was negative({self.accumulator})") #Logs the operation
+        return True
+    
+    def branch_zero(self, addr):
+        '''Branches to a specific location in memory if the accumulator is zero.'''
+        #Sets the current address to the address specified in the instruction if accumulator is "+000000".
+        #It subtracts 1 from the desired address since the program moves to next location upon returning.
+        if self.accumulator == "+000000":
+            self.cur_addr = int(addr) - 1
+            self.log.append(f"Program branched to address: {str(addr).zfill(3)} since the accumulator was zero({self.accumulator})") #Logs the operation
+        return True
+    
+    def halt(self):
+        '''Halts the program.'''
+        self.log.append(f"Program was halted")
+        return False #Returns false to stop the program.
 
 class Simulator:
     '''Holds the list of commands and simulator functions'''
